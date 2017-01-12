e294d4a980e7a04c60f95fab056513ad47c6178b
Mateus Pontes <mateuspo10@gmail.com>
2017-01-11 22:04:50 -0300

    Uses let to local variables

--- test/converter.test.js
+++ test/converter.test.js
@@ -4,9 +4,9 @@ var converter = require("../app/converter");
 describe("Color Code Converter", function() {
 	describe("RGB to Hex conversion", function() {
 		it("converts the basic colors", function() {
-			var redHex   = converter.rgbToHex(255, 0, 0);
-			var greenHex = converter.rgbToHex(0, 255, 0);
-			var blueHex  = converter.rgbToHex(0, 0, 255);
+			let redHex   = converter.rgbToHex(255, 0, 0);
+			let greenHex = converter.rgbToHex(0, 255, 0);
+			let blueHex  = converter.rgbToHex(0, 0, 255);
 
 			expect(redHex).to.equal("ff0000");
 			expect(greenHex).to.equal("00ff00");
@@ -16,9 +16,9 @@ describe("Color Code Converter", function() {
 
 	describe("Hex to RGB conversion", function() {
 		it("converts the basic colors", function() {
-			var redHex   = converter.hexToRgb("ff0000");
-			var greenHex = converter.hexToRgb("00ff00");
-			var blueHex  = converter.hexToRgb("0000ff");
+			let redHex   = converter.hexToRgb("ff0000");
+			let greenHex = converter.hexToRgb("00ff00");
+			let blueHex  = converter.hexToRgb("0000ff");
 
 			expect(redHex).to.deep.equal([255, 0, 0]);
 			expect(greenHex).to.deep.equal([0, 255, 0]);
